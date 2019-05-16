28599b01ccf3cf15c086d2db2b4691b287253eaf
ShaiR&D <Shai.RandD@gmail.com>
2019-05-16 13:07:32 +0300

    fix double sigint

--- 14routesDB/jsonServerMysqlPromise/app.js
+++ 14routesDB/jsonServerMysqlPromise/app.js
@@ -25,20 +25,44 @@ const server = app.listen(port, () => console.log(`Listening on port ${port}!`))
 process.on('SIGTERM', handleSignalEnd)
         .on('SIGINT', handleSignalEnd);
 
+let onlyOnce = false;
 function handleSignalEnd(signal) {
-    console.log(`\nRecieved ${signal}, closing server...`);
-    const serverClosePromise = util.promisify(server.close).bind(server);
-    serverClosePromise()
-    .then(() => {
-        console.log('Server closed');
-        console.log('Closing Mysql connection...');
-        return closeConnection()
-    })
-    .then(() => {
-        console.log('Mysql connection closed');
-        console.log(`Letting node exit on it's own...`);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+    if(!onlyOnce){
+        onlyOnce = true;
+        console.log(`\nRecieved ${signal}, closing server...`);
+        const serverClosePromise = util.promisify(server.close).bind(server);
+        serverClosePromise()
+        .then(() => {
+            console.log('Server closed');
+            console.log('Closing Mysql connection...');
+            return closeConnection()
+        })
+        .then(() => {
+            console.log('Mysql connection closed');
+            console.log(`Letting node exit on it's own...`);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }else console.log(`Recieved ${signal} second time`);
 };
+
+// process.on('SIGTERM', handleSignalEnd)
+//         .on('SIGINT', handleSignalEnd);
+
+// let onlyOnce = false;
+// function handleSignalEnd(signal) {
+//     console.log(`Recieved ${signal} closing server`);
+//     if(!onlyOnce){
+//         onlyOnce = true;
+//         server.close(err => {
+//             err && console.log(err);
+//             console.log('Server closed');
+//             closeConnection(err => {
+//                 err && console.log(err);
+//                 console.log('Mysql connection closed');
+//                 console.log(`Letting node exit on it's own...`);
+//             });
+//         });
+//     }else console.log(`Recieved ${signal} second time`);
+// };
